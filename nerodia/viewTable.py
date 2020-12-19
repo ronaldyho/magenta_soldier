9d6c81b2492250aa55f2d3004e07415aeabd4795
Dark Cowling <42676167+ronaldyho@users.noreply.github.com>
2020-12-19 22:01:50 +0800

    Update viewTable.py

--- nerodia/viewTable.py
+++ nerodia/viewTable.py
@@ -1,1 +1,27 @@
+from xNerodia import *
 
+def browse_extractData( xNerodia_session ):
+    bow = xNerodia_session
+
+    x = bow.iframe(id="mainweb").inner_html
+    print( x )
+
+    y = bow.div(class_name="cont").inner_html
+    print( y )
+
+    #xx = bow.div(class_name="container-right.fr")
+    #print( xx )
+
+    bow.scroll.to( [0,500] )
+
+
+    # //*[@id="ifrm_02"]
+    
+
+    
+
+
+#########
+
+brwFireFox = launch_Simple()
+outPrintTable = browse_extractData(brwFireFox)
